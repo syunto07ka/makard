a92477f6c981e9582951ab7f4df407785adea087
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-04 21:16:52 +0900

    feat: create ListContainer component

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ListContainer from 'containers/pages/List';
 
 const Containers: React.FC = () => (
-    <div>
-      Containers Component
-    </div>
+  <Router>
+    <Switch>
+      <Route path="/">
+        <ListContainer />
+      </Route>
+    </Switch>
+  </Router>
 );
 
 export default Containers;
---
+++ src/containers/pages/List/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const ListContainer: React.FC = () => (
+  <div>
+    List Container Component
+  </div>
+);
+
+export default ListContainer;
