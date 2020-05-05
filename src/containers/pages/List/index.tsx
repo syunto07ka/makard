ecf63cf0c412440ccbca71ee5a27cff456de41c5
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-05 09:20:05 +0900

    feat: make ListContainer content

--- src/containers/pages/List/index.tsx
+++ src/containers/pages/List/index.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
+import Header from 'components/organisms/Header';
+import './style.module.scss';
+import ContentWrapper from 'components/organisms/ContentWrapper';
+
+const iconPath = '../../../logo512.png';
+const displayName = 'カッキー';
+const userId = 'nickname_0310';
 
 const ListContainer: React.FC = () => (
-  <div>
-    List Container Component
-  </div>
+  <React.Fragment>
+    <div styleName="container">
+      <Header displayName={displayName} iconPath={iconPath} userId={userId} />
+      <ContentWrapper title="マイリスト">
+        <div>データがありません</div>
+      </ContentWrapper>
+    </div>
+  </React.Fragment>
 );
 
 export default ListContainer;
