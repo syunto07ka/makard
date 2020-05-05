2510ddbc0a5b536ba9020d65f059402a30c2c23f
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-05 09:21:57 +0900

    feat: path to /profile

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ListContainer from 'containers/pages/List';
+import ProfileContainer from 'containers/pages/Profile';
 
 const Containers: React.FC = () => (
   <Router>
     <Switch>
-      <Route path="/">
+      <Route exact={true} path="/">
         <ListContainer />
+      </Route>
+      <Route path="/profile">
+        <ProfileContainer />
       </Route>
     </Switch>
   </Router>
