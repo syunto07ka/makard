ee38eeadfb09612a3c0cbb92d8d0f0beb7560505
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-05 23:15:32 +0900

    fix: fix ListContainer set Table, surround with PageWrapper

--- src/containers/pages/List/index.tsx
+++ src/containers/pages/List/index.tsx
@@ -1,21 +1,20 @@
 import * as React from 'react';
 import Header from 'components/organisms/Header';
-import './style.module.scss';
 import ContentWrapper from 'components/organisms/ContentWrapper';
+import Table from 'components/organisms/Table';
+import PageWrapper from 'components/templates/PageWrapper';
 
 const iconPath = '../../../logo512.png';
 const displayName = 'カッキー';
 const userId = 'nickname_0310';
 
 const ListContainer: React.FC = () => (
-  <React.Fragment>
-    <div styleName="container">
-      <Header displayName={displayName} iconPath={iconPath} userId={userId} />
-      <ContentWrapper title="マイリスト">
-        <div>データがありません</div>
-      </ContentWrapper>
-    </div>
-  </React.Fragment>
+  <PageWrapper>
+    <Header displayName={displayName} iconPath={iconPath} userId={userId} />
+    <ContentWrapper title="マイリスト">
+      <Table />
+    </ContentWrapper>
+  </PageWrapper>
 );
 
 export default ListContainer;
