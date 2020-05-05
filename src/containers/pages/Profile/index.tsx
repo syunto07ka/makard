5cc258063b5b024a7d0d84c0f630cce0ec11912d
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-05 23:17:51 +0900

    fix: fix ProfileContainer surround with PageWrapper, fix sentence

--- src/containers/pages/Profile/index.tsx
+++ src/containers/pages/Profile/index.tsx
@@ -1,21 +1,19 @@
 import * as React from 'react';
 import Header from 'components/organisms/Header';
-import './style.module.scss';
 import ContentWrapper from 'components/organisms/ContentWrapper';
+import PageWrapper from 'components/templates/PageWrapper';
 
 const iconPath = '../../../logo512.png';
 const displayName = 'カッキー';
 const userId = 'nickname_0310';
 
 const ProfileContainer: React.FC = () => (
-  <React.Fragment>
-    <div styleName="container">
-      <Header displayName={displayName} iconPath={iconPath} userId={userId} />
-      <ContentWrapper title="プロフィール">
-        <div>データがありません</div>
-      </ContentWrapper>
-    </div>
-  </React.Fragment>
+  <PageWrapper>
+    <Header displayName={displayName} iconPath={iconPath} userId={userId} />
+    <ContentWrapper title="プロフィール">
+      <div>Waiting for development</div>
+    </ContentWrapper>
+  </PageWrapper>
 );
 
 export default ProfileContainer;
