c288a85e6b01dc209d5f803595080b4e861d3612
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-05 23:21:24 +0900

    fix: set CreateCard component

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -2,18 +2,22 @@ import * as React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ListContainer from 'containers/pages/List';
 import ProfileContainer from 'containers/pages/Profile';
+import CreateCard from 'containers/modals/CreateCard';
 
 const Containers: React.FC = () => (
-  <Router>
-    <Switch>
-      <Route exact={true} path="/">
-        <ListContainer />
-      </Route>
-      <Route path="/profile">
-        <ProfileContainer />
-      </Route>
-    </Switch>
-  </Router>
+  <React.Fragment>
+    <Router>
+      <Switch>
+        <Route exact={true} path="/">
+          <ListContainer />
+        </Route>
+        <Route path="/profile">
+          <ProfileContainer />
+        </Route>
+      </Switch>
+    </Router>
+    <CreateCard />
+  </React.Fragment>
 );
 
 export default Containers;
